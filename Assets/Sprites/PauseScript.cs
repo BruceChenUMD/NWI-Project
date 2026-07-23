f3c9f9365b78c3aedf863a8567c4d2eb9167c358
BruceChenUMD <leyao09@umd.edu>
2026-07-23 16:32:47 -0400

    pause screen fixed

--- Assets/Sprites/PauseScript.cs
+++ Assets/Sprites/PauseScript.cs
@@ -1,37 +1,75 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class PauseScript : MonoBehaviour
 {
+    [Header("Pause Menu")]
     public GameObject PauseContainer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
+    [SerializeField] private GameObject mainPauseScreen;
+    [SerializeField] private GameObject aboutScreen;
+    [SerializeField] private GameObject leavingScreen;
 
-            PauseContainer.SetActive(true);
-            Time.timeScale = 0f;
-            // Free the mouse cursor to move around the UI
-    Cursor.lockState = CursorLockMode.None;
-    Cursor.visible = true;
-    GetComponent<FirstPersonLook>().enabled = false;
-        }
+    [Header("Player")]
+    [SerializeField] private FirstPersonLook playerLook;
+
+    private bool isPaused;
+
+    private void Start()
+    {
+        SetPaused(false);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SetPaused(!isPaused);
+    }
+
+    private void SetPaused(bool paused)
+    {
+        isPaused = paused;
+
+        if (mainPauseScreen != null)
+            mainPauseScreen.SetActive(true);
+
+        if (aboutScreen != null)
+            aboutScreen.SetActive(false);
+
+        if (leavingScreen != null)
+            leavingScreen.SetActive(false);
+
+        if (PauseContainer != null)
+            PauseContainer.SetActive(paused);
+
+        Time.timeScale = paused ? 0f : 1f;
+
+        if (playerLook != null)
+            playerLook.enabled = !paused;
+
+        Cursor.lockState = paused
+            ? CursorLockMode.None
+            : CursorLockMode.Locked;
+
+        Cursor.visible = paused;
+
+        if (EventSystem.current != null)
+            EventSystem.current.SetSelectedGameObject(null);
+    }
+
     public void ResumeButton()
     {
-        PauseContainer.SetActive(false);
-            Time.timeScale = 1f;
-            // Re-lock the mouse cursor back to the game window
-    Cursor.lockState = CursorLockMode.Locked;
-    Cursor.visible = false;
-    GetComponent<FirstPersonLook>().enabled = true;
+        SetPaused(false);
     }
+
     public void MainMenuButton()
     {
-    UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
-     PauseContainer.SetActive(false);
-            Time.timeScale = 1f;
-    GetComponent<FirstPersonLook>().enabled = true;
+        SetPaused(false);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        SceneManager.LoadScene("Main Menu");
     }
 }
